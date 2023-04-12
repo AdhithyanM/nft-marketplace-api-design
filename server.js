f5403c9dd5de7aee565db944e504d0d8cbacbc3c
AdhithyanM <adhithyanmanoharan26@gmail.com>
2023-04-12 22:22:54 +0530

    connecting to atlas and creating nft model

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const app = require("./app");
 
 dotenv.config({ path: "./config/config.env" });
+
+const DB_URI = process.env.DATABASE_URI.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB_URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+  })
+  .then((con) => {
+    console.log(
+      `Successfully connected to the database ${con.connection.host}`
+    );
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
